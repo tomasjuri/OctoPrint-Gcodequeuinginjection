fe9e8621fabce351127964b95f894d9c903f2d1c
Tomas Jurica <tom@veracityprotocol.org>
2025-08-27 13:24:29 +0200

    Add name amd other properties to the plugin.

--- octoprint_GcodeQueuingInjection/__init__.py
+++ octoprint_GcodeQueuingInjection/__init__.py
@@ -7,8 +7,9 @@ from .GcodequeuinginjectionPlugin import GcodequeuinginjectionPlugin
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Gcodequeuinginjection Plugin"
-
+__plugin_name__ = "GcodeQueuingInjection"
+__plugin_version__ = "0.1.0"
+__plugin_description__ = "Plugin for automatic layer capture during 3D printing."
 
 # Set the Python version your plugin is compatible with below. Recommended is Python 3 only for all new plugins.
 # OctoPrint 1.4.0 - 1.7.x run under both Python 3 and the end-of-life Python 2.
